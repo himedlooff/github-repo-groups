51ddaf1249fa89548cfcb93bebd71acaf68f898d
Mike Morici <Michael.Morici@cfpb.gov>
2014-07-23 12:45:00 -0400

    Add all repos and search only all repos.

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 function RepoListCtrl($scope, $filter, $http) {
   $scope.setList = [];
+  $scope.repoList = [];
   $scope.search = '';
+  
   $http.get('repos.json').
     success(function(data){
       $scope.setList = data.sets;
+      console.log(data.sets);
+    }).
+    error(function(){
+      alert('Error');
+    });
+
+  $http.get('https://api.github.com/orgs/cfpb/repos?per_page=100').
+    success(function(data){
+      $scope.repoList = data;
+      console.log(data);
     }).
     error(function(){
       alert('Error');
