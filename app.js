cd18e30875e601a3c944fefd3379ad69bd4f76c8
Mike Morici <Michael.Morici@cfpb.gov>
2014-07-24 19:56:54 -0400

    An example of getting a file.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ function RepoListCtrl($scope, $filter, $http) {
   $scope.setList = [];
   $scope.repoList = [];
   $scope.search = '';
+  $scope.bowerData = '';
   
   $http.get('repos.json').
     success(function(data){
@@ -14,8 +15,24 @@ function RepoListCtrl($scope, $filter, $http) {
   $http.get('https://api.github.com/orgs/cfpb/repos?per_page=100').
     success(function(data){
       $scope.repoList = data;
+      $scope.getBowerJson();
     }).
     error(function(){
       alert('Error');
     });
-}
+
+  $scope.getBowerJson = function() {
+    for (var i = 0; i < $scope.repoList.length; i++) {
+      if ($scope.repoList[i].name == 'cf-expandables') {
+        $http.get($scope.repoList[i].contents_url.replace('{+path}', 'bower.json')).
+          success(function(data){
+            $scope.bowerData = JSON.parse(atob(data.content));
+            // console.log($scope.bowerData.keywords);
+          }).
+          error(function(){
+            alert('Error, no bower.json');
+          });
+      }
+    }
+  };
+};
